606cc9d06d7bea293a42fe77d9e707beee20e12b
jastylr <jtaylor@faultlinemedia.com>
2015-04-21 21:58:52 -0700

    Add new project images

--- js/helper.js
+++ js/helper.js
@@ -20,6 +20,7 @@ var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLlinkedIn = '<li class="flex-item"><span class="orange-text">linkedin</span><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
@@ -41,6 +42,7 @@ var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectURL = '<a href="%data%" target="_blank">Launch Site</a>';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
@@ -85,7 +87,7 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  logClicks(loc.pageX, loc.pageY);
 });
 
 
@@ -233,11 +235,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+  //Make sure the map bounds get updated on page resize
+ map.fitBounds(mapBounds);
+});
---
+++ js/resumeBuilder.js
@@ -0,0 +1,251 @@
+
+// Bio object
+var bio = {
+	"name" : "Jason Taylor",
+	"role" : "SF Bay Area Front-end Web Developer",
+	"contacts" : {
+		"email" : "jtaylor@faultlinemedia.com",
+		"mobile" : "650-759-2009",
+		"github" : "https://github.com/jastylr",
+		"linkedin" : "https://www.linkedin.com/in/jasonetaylor1",
+		"location" : "Redwood City, CA"
+	},
+	"welcomeMessage": "<p>My name is Jason Taylor and I am a web developer living and working in the San Francisco Bay Area.<\/p>\
+	<p>Feel free to look around my interactive resume and contact me for any open positions.<\/p>",
+	"skills": ["HTML", "CSS", "Javascript", "JQuery", "AJAX", "Responsive design", "Photoshop"],
+	"biopic": "images/jason-taylor.jpg"
+};
+
+var work = {
+	"jobs": [
+		{
+			"employer": "Faultline Media Solutions",
+			"title": "Web Designer",
+			"location": "Redwood City, CA",
+			"dates": "2006-Present",
+			"description": "<ul> \
+				<li>Designed websites using hand-coded HTML and Cascading Style sheets (CSS).<\/li> \
+				<li>Used PHP to create dynamic web pages that access back-end MySQL databases.<\/li> \
+				<li>Created websites using the Drupal content management system and providing additional functionality through custom-coded PHP modules.<\/li> \
+				<li>Used Adobe Photoshop and Illustrator to design site UI mockups and web graphics that were then sliced and optimized in preparation to create CSS based site layouts.<\/li> \
+				<li>Used JQuery, JavaScript to add client-side browser interactivity.<\/li> \
+				<li>Created graphics for print media such as brochures, flyers and postcards and worked with print houses to ensure quality results.<\/li><\/ul> \
+				<p>HTML, CSS, PHP, MySQL, JQuery, JavaScript, AJAX, Twitter Bootstrap, Codeigniter, Drupal CMS, ASP.NET/SQL Server, Git, GitHub<\/p> \
+				<a href=\"http:\/\/www.faultlinemedia.com\" target=\"_blank\">www.faultlinemedia.com<\/a>"
+		},
+		{
+			"employer" : "Coding Dojo",
+			"title" : "Instuctor",
+			"location" : "Mountain View, CA",
+			"dates" : "September, 2014 - December, 2014",
+			"description" : "<p>Provided instruction to students at a Full-stack Web Development bootcamp</p>"
+		},
+		{
+			"employer" : "Excite@Home",
+			"title" : "Advanced Software Engineer",
+			"location" : "Redwood City, CA",
+			"dates" : "August, 2000 - January, 2002",
+			"description" : "<ul><li>Used HTML, JAVA, Perl and SQL to create and maintain an application used by the Excite@Home engineering operations group to manage TELNET, IOS and SNMP authentication data on all devices across the @Home network.</\li> \
+				<li>Worked on the Network Management Database (NMDB), which consisted of a large-scale Oracle database and many Perl modules used to gather data from the entire Excite@Home network backbone.<\/li> \
+				<li>Created a shared library written in C utilizing the Oracle OCI (Oracle Call Interface) functions to allow external databases within the organization to write and retrieve specific information to and from the network management database (NMDB).<\/li> \
+				<li>Used Object Oriented Perl to create packages for various network devices by device type which were used to access the devices via TELNET/IOS or SNMP and retrieve configuration data as well as update authentication information.<\/li><\/ul>"
+		},
+		{
+			"employer" : "Verilink Corporation",
+			"title" : "Software Engineer III",
+			"location" : "San Jose, CA",
+			"dates" : "1997 - 1999",
+			"description" : "<ul><li>Responsible for porting existing Windows 3.x network management code to the Win95 platform using C++ and the Microsoft Foundation Classes.<\/li> \
+				<li>Responsible for the continued development and maintenance of a large-scale network management application for Win95 with emphasis on GUI interface code.<\/li> \
+				<li>Used object oriented design techniques to create several base class software modules used to provide multiple hardware module support while providing a common user interface look and feel.<\/li> \
+				<li>Used SQL, ODBC and Oracle 8 Personal and Server versions to provide database support to the current NMS application.<\/li> \
+				<li>Interfaced with embedded systems’ engineers to develop a common API for the management of multiple hardware modules.<\/li><\/ul>"
+		},
+		{
+			"employer" : "Verilink Corporation",
+			"title" : "Software Engineer II",
+			"location" : "San Jose, CA",
+			"dates" : "1993 - 1995",
+			"description" : "<ul><li>Responsible for the design, implementation and debugging of various software modules comprising a large-scale Windows 3.x network management application written in C++ and the Microsoft Foundation Classes.<\/li> \
+				<li>Utilized graphical skills to create computer images used in the development of the current Windows 3.x management application.<\/li> \
+				<li>Created a graphical software application (Test System 2000) that worked in conjunction with specific hardware modules to provide T1 test set functionality. The application later received the 1995 International Communications Association (ICA) Award for Innovative Product of the Year.<\/li><\/ul>"
+		}
+	]
+};
+
+var education = {
+	"schools": [
+		{
+			"name" : "The Coding Dojo",
+			"location" : "Mountain View, CA",
+			"degree" : "N/A",
+			"majors" : [''],
+			"dates" : 2014,
+			"url" : ""
+		},
+		{
+			"name": "University of Santa Cruz Extension",
+			"location": "Santa Clara, CA",
+			"degree" : "N/A",
+			"majors" : [''],
+			"dates": 2001,
+			"url" : ""
+		},
+		{
+			"name": "College of San Mateo",
+			"location": "San Mateo, CA",
+			"degree" : "N/A",
+			"majors" : [''],
+			"dates": 1989,
+			"url" : ""
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title": "Front-end Web Development Nanodegree",
+			"school": "Udacity",
+			"date": 2015,
+			"url" : "http://www.udacity.com"
+		}
+	]
+};
+
+// Projects - added URL and Technologies properties
+var projects = {
+	"project" : [
+		{
+			"title": "CrisisGuard",
+			"dates": "2014",
+			"description": "A responsive HTML/CSS site",
+			"images": ["images/projects/thumb/crisisguard-d.jpg"],
+			"url": "http://www.crisisguard.com",
+			"technologies": ["HTML", "CSS"]
+		},
+		{
+			"title": "NoXcuses Fitness",
+			"dates": "2014",
+			"description": "A responsive HTML/CSS site",
+			"images": ["images/projects/thumb/noxcusesfitness-d.jpg"],
+			"url": "http://www.noxcusesfitness.com",
+			"technologies": ["HTML", "CSS"]
+		}
+	]
+};
+
+// Display method used to output bio information to the screen
+bio.display = function() {
+	$('#header').prepend(HTMLheaderRole.replace('%data%', bio.role));
+	$('#header').prepend(HTMLheaderName.replace('%data%', bio.name));
+	$('#header').append(HTMLbioPic.replace('%data%', bio.biopic));
+	$('#header').append(HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage));
+
+	var bioMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
+	var bioEmail = HTMLemail.replace('%data%', bio.contacts.email);
+	var bioGithub = HTMLgithub.replace('%data%', makeLink(bio.contacts.github));
+	var bioLinkedIn = HTMLlinkedIn.replace('%data%', makeLink(bio.contacts.linkedin));
+	var bioLocation = HTMLlocation.replace('%data%', bio.contacts.location);
+	var formattedContacts = bioMobile + bioEmail + bioGithub + bioLinkedIn + bioLocation;
+	$('#topContacts, #footerContacts').append(formattedContacts);
+
+	// Check if the skills array is empty or not and if not, output the skills
+	if (bio.skills.length > 0) {
+		$('#header').append(HTMLskillsStart);
+		for(skill in bio.skills) {
+			$('#skills').append(HTMLskills.replace('%data%', bio.skills[skill]));
+		}
+	}
+}
+
+projects.display = function() {
+	for (var project in projects.project) {
+		$('#projects').append(HTMLprojectStart);
+
+		var projectTitle = HTMLprojectTitle.replace('%data%', projects.project[project].title);
+		$('.project-entry:last').append(projectTitle);
+
+		var projectDate = HTMLprojectDates.replace('%data%', projects.project[project].dates);
+		$('.project-entry:last').append(projectDate);
+
+		var projectDesc = HTMLprojectDescription.replace('%data%', projects.project[project].description);
+		$('.project-entry:last').append(projectDesc);
+
+		if (projects.project[project].images.length > 0) {
+			for (image in projects.project[project].images) {
+				var image = HTMLprojectImage.replace('%data%', projects.project[project].images[image]);
+				$('.project-entry:last').append(image);
+			}
+		}
+
+		var projectURL = HTMLprojectURL.replace('%data%', projects.project[project].url);
+		$('.project-entry:last').append(projectURL);
+	}
+}
+
+work.display = function() {
+	for (job in work.jobs) {
+		$('#workExperience').append(HTMLworkStart);
+
+		var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace('%data%', work.jobs[job].title);
+		var formatedEmployerTitle = formattedEmployer + formattedTitle;
+
+		$('.work-entry:last').append(formatedEmployerTitle);
+		$('.work-entry:last').append(HTMLworkDates.replace('%data%', work.jobs[job].dates));
+		$('.work-entry:last').append(HTMLworkLocation.replace('%data%', work.jobs[job].location));
+		$('.work-entry:last').append(HTMLworkDescription.replace('%data%', work.jobs[job].description));
+	}
+}
+
+education.display = function() {
+	$('#education').append(HTMLschoolStart);
+	for (school in education.schools) {
+		var formattedName = HTMLschoolName.replace('%data%', education.schools[school].name);
+		var formattedDegree = HTMLschoolDegree.replace('%data%', education.schools[school].degree);
+		$('.education-entry:last').append(formattedName + formattedDegree);
+		$('.education-entry:last').append(HTMLschoolDates.replace('%data%', education.schools[school].dates));
+		$('.education-entry:last').append(HTMLschoolLocation.replace('%data%', education.schools[school].location));
+
+		if (education.schools[school].majors.length > 0) {
+			for (major in education.schools[school].majors) {
+				$('.education-entry:last').append(HTMLschoolMajor.replace('%data%', education.schools[school].majors[major]));
+			}
+		}
+	}
+}
+
+// makeLink - Used to take a web address and convert it into a clickable link
+// with optional display text
+function makeLink(address, text, strip_protocol=true, new_window=true) {
+	
+	if (!address) {
+		return;
+	}
+
+	var regex_prot = /^(.*?):\/\/(www.)?/,
+			link = '';
+	
+	if(text && strip_protocol === true) {
+		text = text.replace(regex_prot, '');
+	}
+	else {
+		text = address.replace(regex_prot, '');
+	}
+	
+	// Build the link. If no text was specified, then use the address as the link text
+	link = '<a href="' + address + ((new_window === true) ? '" target="_blank">' : '">') + 
+					((text !== undefined) ? text : address) + '</a>';
+	
+	return link;
+}
+
+// Call functions to display the various resume sections
+bio.display();
+work.display();
+projects.display();
+education.display();
+
+// $('#workExperience').click(function() {
+// 	$('.work-entry').slideToggle();
+// });
+
+$('#mapDiv').append(googleMap);
