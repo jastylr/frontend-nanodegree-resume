7cd363d093de7af7792b25772a31e1889b0056a7
jastylr <jtaylor@faultlinemedia.com>
2015-05-02 10:11:33 -0700

    Update helper.js to wrap contacts in div tags

--- js/helper.js
+++ js/helper.js
@@ -15,14 +15,14 @@ replace the %data% placeholder text you see in them.
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr/>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLlinkedIn = '<li class="flex-item"><span class="orange-text">linkedin</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item"><div><span class="orange-text">%contact%</span><span class="white-text">%data%</span></div></li>';
+var HTMLmobile = '<li class="flex-item"><div><span class="orange-text">mobile</span><span class="white-text">%data%</span></div></li>';
+var HTMLemail = '<li class="flex-item"><div><span class="orange-text">email</span><span class="white-text">%data%</span></div></li>';
+var HTMLtwitter = '<li class="flex-item"><div><span class="orange-text">twitter</span><span class="white-text">%data%</span></div></li>';
+var HTMLgithub = '<li class="flex-item"><div><span class="orange-text">github</span><span class="white-text">%data%</span></div></li>';
+var HTMLlinkedIn = '<li class="flex-item"><div><span class="orange-text">linkedin</span><span class="white-text">%data%</span></div></li>';
+var HTMLblog = '<li class="flex-item"><div><span class="orange-text">blog</span><span class="white-text">%data%</span></div></li>';
+var HTMLlocation = '<li class="flex-item"><div><span class="orange-text">location</span><span class="white-text">%data%</span></div></li>';
 
 var HTMLbioPic = '<div class="col-sm-3 col-lg-2"><div class="biopic"><img src="%data%"></div></div>';
 var HTMLwelcomeMsg = '<div class="col-sm-9 col-lg-10"><span class="welcome-message">%data%</span></div>';
